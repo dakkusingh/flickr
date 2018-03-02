31f8e859abe8330bd5531b5cc19d74d02fcda59d
dakku <dakku@97634.no-reply.drupal.org>
2018-03-02 23:50:04 +0000

    Move Flickr API Services to a dedicated module.

--- src/Form/FlickrSettings.php
+++ src/Form/FlickrSettings.php
@@ -36,43 +36,10 @@ class FlickrSettings extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('flickr.settings');
 
-    $form['credentials'] = [
-      '#type' => 'fieldset',
-      '#title' => $this->t('OAuth Settings'),
-    ];
-
-    $form['credentials']['help'] = [
-      '#type' => '#markup',
-      '#markup' => $this->t('API Key from Flickr. Get an API Key at @link.',
-        [
-          '@link' => Link::fromTextAndUrl('https://www.flickr.com/services/apps/create/apply',
-          Url::fromUri('https://www.flickr.com/services/apps/create/apply'))->toString(),
-        ]
-      ),
-    ];
-
-    $form['credentials']['api_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('API Key'),
-      '#default_value' => $config->get('api_key'),
-    ];
-
-    $form['credentials']['api_secret'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('API key secret'),
-      '#default_value' => $config->get('api_secret'),
-    ];
-
     $form['flickr'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Flickr Settings'),
       '#description' => $this->t('The following settings connect Flickr module with external APIs.'),
-    ];
-
-    $form['flickr']['host_uri'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Flickr URL'),
-      '#default_value' => $config->get('host_uri'),
     ];
 
     $form['flickr']['api_uri'] = [
@@ -89,9 +56,6 @@ class FlickrSettings extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('flickr.settings')
-      ->set('api_key', $form_state->getValue('api_key'))
-      ->set('api_secret', $form_state->getValue('api_secret'))
-      ->set('host_uri', $form_state->getValue('host_uri'))
       ->set('api_uri', $form_state->getValue('api_uri'))
       ->save();
 
